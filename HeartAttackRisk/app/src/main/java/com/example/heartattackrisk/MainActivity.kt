defd43f8d5a1e684b1128acdcd6c6a5d6e347ddf
LucasBrentano <brentano.l@gmail.com>
2022-09-25 14:19:07 -0300

    Buton2

--- HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity.kt
+++ HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity.kt
@@ -12,13 +12,28 @@ class MainActivity : AppCompatActivity() {
 
         val sum_value = 0
         val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
 
-
-        button1.setOnClickListener(){
+        button1.setOnClickListener() {
             val sum = Sum(sum_value + 1)
-            val intent = Intent(this, MainActivity2::class.java)
-            intent.putExtra("sum", sum)
-            startActivity(intent)
+
+            button1.setOnClickListener() {
+                val sum = Sum(sum_value + 1)
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("sum", sum)
+                startActivity(intent)
+            }
+
+            button2.setOnClickListener() {
+                val sum = Sum(sum_value + 2)
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("sum", sum)
+                startActivity(intent)
+            }
         }
 
     }
--- HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/PassFunction.kt
+++ HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/PassFunction.kt
@@ -1,8 +1,14 @@
 package com.example.heartattackrisk
 
+import android.content.Intent
+
 class PassFunction {
 }
 
-fun transfer(value: Int){
-
+fun transfer(sum: Sum ,currentActivity: Package, targetActivity: Package){
+//    val intent = Intent(currentActivity, targetActivity::class.java)
+//jj    val sum = Sum(sum_value + 1)
+//    val intent = Intent(this, MainActivity2::class.java)
+//    intent.putExtra("sum", sum)
+//    startActivity(intent)
 }
