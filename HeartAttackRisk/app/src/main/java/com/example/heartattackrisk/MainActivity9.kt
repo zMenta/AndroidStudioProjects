9d8b84be0d6d288bb00b3851966fe8ebb44b921f
LucasBrentano <brentano.l@gmail.com>
2022-09-25 17:00:56 -0300

    main9

---
+++ HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity9.kt
@@ -0,0 +1,29 @@
+package com.example.heartattackrisk
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity9 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_8)
+        val sum = intent.getParcelableExtra<Sum>("sum")
+
+        val text = findViewById<TextView>(R.id.result)
+        val button1 = findViewById<Button>(R.id.button1)
+
+
+        if (sum != null) {
+            text.text = sum.value.toString()
+
+            button1.setOnClickListener() {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
+}
