182ac321d9ed4dcd9c3f1fcd9dccd51ef590f755
LucasBrentano <brentano.l@gmail.com>
2022-09-25 13:59:07 -0300

    BasicActivittyChange

--- HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity.kt
+++ HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity.kt
@@ -10,12 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sum_value = 0
         val button1 = findViewById<Button>(R.id.button1)
 
+
         button1.setOnClickListener(){
-//            val sum = Sum(0)
+            val sum = Sum(sum_value + 1)
             val intent = Intent(this, MainActivity2::class.java)
-//            intent.putExtra("sum", sum)
+            intent.putExtra("sum", sum)
             startActivity(intent)
         }
 
--- HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity2.kt
+++ HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.heartattackrisk
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+        val sum = intent.getParcelableExtra<Sum>("sum")
+
+        val text = findViewById<TextView>(R.id.title)
+
+        if (sum != null) {
+            text.text = sum.value.toString()
+        }
+
     }
 }
