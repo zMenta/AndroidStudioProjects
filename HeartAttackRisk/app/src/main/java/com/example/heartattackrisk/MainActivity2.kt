3461deb5320992281aae7b54de832c95977d22b0
LucasBrentano <brentano.l@gmail.com>
2022-09-25 15:56:10 -0300

    Fix

--- HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity2.kt
+++ HeartAttackRisk/app/src/main/java/com/example/heartattackrisk/MainActivity2.kt
@@ -6,7 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity3 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
